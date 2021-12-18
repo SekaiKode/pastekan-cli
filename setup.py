3bbc8aff7350ca51a82e038cf88a3c8d07ac61ea
madewgn <mad.ewgn2@gmail.com>
2021-12-18 19:56:51 +0800

    upadte txt

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except ImportError:
 
 setup(
     name='pastekan',
-    version='0.2',
+    version='0.3',
     url='https://github.com/SekaiKode/pastekan-cli',
     description='CLI client for pastekan server (pastekan.cf) written in Python.',
     long_description=read_md('README.md'),
