e73d6cdad830ff86bb94ff9846442be0d2b5b5b6
madewgn <mad.ewgn2@gmail.com>
2021-12-18 17:07:47 +0800

    update link

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except ImportError:
 
 setup(
     name='pastekan',
-    version='0.1',
+    version='0.2',
     url='https://github.com/SekaiKode/pastekan-cli',
     description='CLI client for Haste server (hastebin.com) written in Python.',
     long_description=read_md('README.md'),
